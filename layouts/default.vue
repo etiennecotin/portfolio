f1199f008cf5884e4f23faa14eb35e09c1840f1b
Etienne <etienne.cotin@gmail.com>
2021-02-05 11:53:39 +0100

    ✨ Add scroll top

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,12 +9,15 @@
 <script>
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
-import scrollTop from '~/components/generic/scrollTop'
+import scrollTop from '~/components/layout/scrollTop'
 export default {
   components: {
     Header,
     Footer,
     scrollTop,
+  },
+  mounted() {
+    this.$store.commit('setLoading', true)
   },
 }
 </script>
